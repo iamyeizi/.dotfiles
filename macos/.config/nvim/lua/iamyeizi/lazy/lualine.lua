e27c53a3e10a0e84341da01203985e16176c389f
iamyeizi <37552350+iamyeizi@users.noreply.github.com>
2024-04-16 17:28:07 -0300

    neovim refactor =P

---
+++ macos/.config/nvim/lua/iamyeizi/lazy/lualine.lua
@@ -0,0 +1,16 @@
+
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				icons_enabled = true,
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_c = { { "filename", path = 3 } },
+			},
+		},
+	},
+}
---
+++ macos/.config/nvim/lua/iamyeizi/lazy/vimbegood.lua
@@ -0,0 +1,12 @@
+return {
+    "ThePrimeagen/vim-be-good",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+    end
+}
+
+
