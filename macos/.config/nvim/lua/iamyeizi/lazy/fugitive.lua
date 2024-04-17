97c4843aaa6f8489846438b8bfacaa1588fff813
iamyeizi <37552350+iamyeizi@users.noreply.github.com>
2024-04-16 21:07:58 -0300

    neovim: fugitive setup

--- macos/.config/nvim/lua/iamyeizi/lazy/fugitive.lua
+++ macos/.config/nvim/lua/iamyeizi/lazy/fugitive.lua
@@ -1,7 +1,6 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-
         -- Git Status
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
@@ -18,26 +17,27 @@ return {
 
                 -- Git Push
                 local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
+                local opts = { buffer = bufnr, remap = false }
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
                 end, opts)
 
+                -- NOTE: Should use when collaborating with others on the same branch
                 -- Git Pull Rebase
                 vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
+                    vim.cmd.Git({ 'pull', '--rebase' })
                 end, opts)
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
-
-                -- Git Push Origin
+                -- Git Push set origin
                 vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
             end,
         })
 
-
+        -- Left diff
         vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
+        -- Right diff
         vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
     end
 }
