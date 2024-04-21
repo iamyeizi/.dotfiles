316245b38c11fb939bb94c5f30c0e3758c9cee7e
iamyeizi <37552350+iamyeizi@users.noreply.github.com>
2024-04-21 04:46:18 -0300

    taptoclick & nvim bg none

--- home/.config/nvim/lua/iamyeizi/lazy/colors.lua
+++ home/.config/nvim/lua/iamyeizi/lazy/colors.lua
@@ -1,12 +1,12 @@
--- function SetTheme(color)
--- 	color = color or "rose-pine"
--- 	vim.cmd.colorscheme(color)
---
--- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---
--- end
---
+function SetTheme(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
 return {
 
     {
@@ -22,7 +22,7 @@ return {
 
             vim.cmd("colorscheme rose-pine")
 
-            -- SetTheme()
+            SetTheme()
         end
     },
 
